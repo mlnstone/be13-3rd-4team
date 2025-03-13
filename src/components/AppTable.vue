831574acffc49e2d5173f06febeb3e1c382f291a
MSP_31 <jhss9747@gmail.com>
2025-03-13 15:59:59 +0900

    게시판 조회 수정

--- src/components/AppTable.vue
+++ src/components/AppTable.vue
@@ -4,6 +4,7 @@
     <div class="mt-6">
       <div class="my-6 overflow-hidden bg-white rounded-md shadow">
         <table class="w-full text-left border-collapse">
+          <!-- 제목 -->
           <thead class="border-b">
             <tr>
               <th
@@ -28,8 +29,13 @@
               </th>
             </tr>
           </thead>
+          <!-- 내용 -->
           <tbody>
-            <tr v-for="(post, i) in posts" :key="i" class="hover:bg-gray-200">
+            <tr
+              v-for="(post, i) in paginatedPosts"
+              :key="i"
+              class="hover:bg-gray-200"
+            >
               <td class="px-6 py-4 text-lg text-gray-500 border-b">
                 {{ post.postNo }}
               </td>
@@ -45,6 +51,20 @@
             </tr>
           </tbody>
         </table>
+        <!-- 페이징 버튼 -->
+        <div
+          class="flex flex-col items-center px-5 py-5 bg-white border-t xs:flex-row xs:justify-between"
+        >
+          <div class="flex mr-4 rounded">
+            <a
+              v-for="page in totalPages"
+              :key="page"
+              @click="setPage(page)"
+              class="px-3 py-2 leading-tight text-indigo-700 bg-white border border-r-0 border-gray-200 hover:bg-indigo-500 hover:text-white"
+              ><span>{{ page }}</span></a
+            >
+          </div>
+        </div>
       </div>
     </div>
   </div>
@@ -57,6 +77,29 @@ export default {
     posts: {
       type: Array,
       required: true,
+    },
+  },
+  data() {
+    return {
+      currentPage: 1,
+      itemsPerPage: 10,
+    };
+  },
+  computed: {
+    paginatedPosts() {
+      // 현재 페이지 번호에 따라 시작 인덱스를 계산합니다.
+      const start = (this.currentPage - 1) * this.itemsPerPage;
+      // posts 배열에서 해당 범위의 게시물을 추출합니다.
+      return this.posts.slice(start, start + this.itemsPerPage);
+    },
+    totalPages() {
+      // 전체 게시물 수에 따라 총 페이지 수를 계산합니다.
+      return Math.ceil(this.posts.length / this.itemsPerPage);
+    },
+  },
+  methods: {
+    setPage(page) {
+      this.currentPage = page;
     },
   },
 };
