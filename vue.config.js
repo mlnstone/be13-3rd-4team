a97e0249e3f24d3b124ec5ff612c2bf4fc8824dc
gjaku1031 <150967087+gjaku1031@users.noreply.github.com>
2025-03-15 16:47:19 +0900

    Update vue.config.js

--- vue.config.js
+++ vue.config.js
@@ -1,6 +1,3 @@
-const { defineConfig } = require("@vue/cli-service");
-module.exports = defineConfig({
-  transpileDependencies: true,
-
-  // outputDir: "../be13-2nd-4team/src/main/resources/static",
-});
+module.exports = {
+  transpileDependencies: true
+}
