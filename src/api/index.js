26d8830ae8f09326880ecc4ff1436e809d360b2d
gjaku1031 <150967087+gjaku1031@users.noreply.github.com>
2025-03-15 16:34:58 +0900

    Update index.js

--- src/api/index.js
+++ src/api/index.js
@@ -1,11 +1,12 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'http://183.99.3.15:8087',
+  baseURL: process.env.VUE_APP_API_URL || "http://183.99.3.15:8087", // 기본값 설정
   headers: {
-    'Content-Type': 'application/json',
+    "Content-Type": "application/json",
+    "Accept": "application/json"  // 응답 데이터 타입 명시
   },
-  withCredentials: true
+  withCredentials: true // CORS 요청 시 인증정보 포함
 });
 
 export default api;
