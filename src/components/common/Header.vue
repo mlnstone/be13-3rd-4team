9190f10e14ff66e9b255af8c9785e07a6f304621
MSP_31 <jhss9747@gmail.com>
2025-03-26 19:11:38 +0900

    [홍재민] 아이콘, 이름 변경

--- src/components/common/Header.vue
+++ src/components/common/Header.vue
@@ -10,7 +10,7 @@
     <header class="d-flex flex-wrap justify-content-center py-3 mb-4 border-bottom">
         <a href="/" class="d-flex align-items-center mb-3 mb-md-0 me-md-auto link-body-emphasis text-decoration-none">
             <img src="@/assets/moyobom.svg" alt="모요봄 아이콘" />
-            <span class="fs-4">MOYO</span>
+            <span class="fs-4">MOYOBOM</span>
         </a>
 
         <ul class="nav nav-pills">
